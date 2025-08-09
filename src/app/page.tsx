f10b5559f5e579a9c6b61fada26993be75e90a98
Mitts For Code <jorgelogomez@gmail.com>
2025-08-09 14:02:23 +0200

    more content and style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,30 +3,47 @@ export default function LandingPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-8 bg-gray-900 text-white">
       <div className="text-center">
+        <img
+          src="/assets/logo.svg"
+          alt="Mitts For Code Logo"
+          className="mx-auto mb-8 h-40" // La clase 'mx-auto' de Tailwind CSS centra el elemento
+        />
         <h1 className="text-4xl font-bold mb-4">Mitts For Code</h1>
         <p className="text-xl text-gray-300 mb-8">
-          A web to show apps made with Mitts For Code
+          A web to show apps made by Mitts For Code
         </p>
         <hr className="mb-4 mt-4" />
-        <h2 className="text-4xl font-bold mb-4">MindRook</h2>
-        <p className="text-xl text-gray-300 mb-8">
-          Sharpen your tactical vision with unlimited chess puzzles.
-        </p>
-        <a
-          href="https://play.google.com/store/apps/details?id=com.owldotask.chesspuzzles" // <-- CAMBIA ESTO POR TU URL REAL DE LA APP
-          className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg transition-colors"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Get it on Google Play
-        </a>
-        <div className="mt-12">
+        <div className="bg-gray-700 rounded-lg p-4">
+          <img
+            src="/assets/mindrook/logo.png"
+            alt="Mitts For Code Logo"
+            className="mx-auto mb-8 " // La clase 'mx-auto' de Tailwind CSS centra el elemento
+          />{' '}
+          <p className="text-xl text-gray-300 mb-8">
+            Sharpen your tactical vision with chess puzzles
+          </p>
           <a
-            href="/mindrook/privacy-policy"
-            className="text-gray-400 hover:text-white underline"
+            href="https://play.google.com/store/apps/details?id=com.owldotask.chesspuzzles" // <-- CAMBIA ESTO POR TU URL REAL DE LA APP
+            className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg transition-colors mb-8 block"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            Privacy Policy
+            Get it on Google Play
           </a>
+          <div className="">
+            <a
+              href=""
+              className="text-gray-400 hover:text-white underline mr-4"
+            >
+              Info
+            </a>
+            <a
+              href="/mindrook/privacy-policy"
+              className="text-gray-400 hover:text-white underline"
+            >
+              Privacy Policy
+            </a>
+          </div>
         </div>
       </div>
     </main>
